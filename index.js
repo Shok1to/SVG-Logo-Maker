e6482353ffa25d97cf6c7d3aa2fe1c417f541784
Shok1to <shoko.takahashi.911@gmail.com>
2023-05-24 18:34:39 -0400

    created class for shapes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //Include packages needed for this application
-const inquirer = require('inquirer')
-const fs = require('fs')
-const path = require('path')
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // Create an array of questions for user input
 const questions =[
@@ -9,21 +8,34 @@ const questions =[
         type:'input',
         message:'Please enter text for your logo, it is up to three characters',
         name:'text',
-    }
+    },
     {
         type:'input',
         message:'Please choose the text color, enter a color keyword or a hexadecimal number',
         name:'textColor',
-    }
+    },
     {
         type:'input',
         message:'Please choose a shape of your logo from: circle, triangle, and square',
         name:'logoShape',
         choices: ['circle','triangle','square'],
-    }
+    },
     {
         type:'input',
         message:'Please choose the logo color, enter a color keyword or a hexadecimal number',
         name: logoColor,
     }
 ];
+
+//Create a function to initialize app
+function init{
+    inquirer.prompt(questions)
+    .then(function(answers){
+        console.log(answers)
+
+        fs.writeFileSync('./logo.svg',generateMarkdown({...answers}))
+    })
+}
+
+// Function call to initialize app
+init();
